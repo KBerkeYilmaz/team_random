aab1286dde0f819e0ff66d23004e98c804f1d3da
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2024-04-03 15:21:38 +0300

    UPDATE: Landing page design iterations#1.

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -5,8 +5,40 @@ import { ModeToggle } from "@/components/ModeToggle";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-screen">
-      <Counter />
+    <main className="flex flex-col w-screen justify-start items-center">
+      {/* <Counter /> */}
+      <section id="hero" className="flex w-screen justify-center items-center min-h-screen">
+      <div className="w-fit">
+        <h1 className="poppins-bold text-6xl text-left text-pretty antialiased dark:text-orange-400 tracking-wider">
+          Coding your digital future <br />
+          <span className="pixelify-header-bold text-6xl">pixel by pixel</span>
+        </h1>
+      </div>
+      </section>
+      <section id="service" className="flex w-screen justify-center items-center min-h-screen">
+      <div className="w-fit">
+        <h1 className="poppins-bold text-6xl text-left text-pretty antialiased dark:text-orange-400 tracking-wider">
+          This is our services <br />
+          <span className="pixelify-header-bold text-6xl">pixel by pixel</span>
+        </h1>
+      </div>
+      </section>
+      <section id="about" className="flex w-screen justify-center items-center min-h-screen">
+      <div className="w-fit">
+        <h1 className="poppins-bold text-6xl text-left text-pretty antialiased dark:text-orange-400 tracking-wider">
+          This is about (kinda) <br />
+          <span className="pixelify-header-bold text-6xl">pixel by pixel</span>
+        </h1>
+      </div>
+      </section>
+      <section id="cta" className="flex w-screen justify-center items-center min-h-screen">
+      <div className="w-fit">
+        <h1 className="poppins-bold text-6xl text-left text-pretty antialiased dark:text-orange-400 tracking-wider">
+          Call to action gives us money <br />
+          <span className="pixelify-header-bold text-6xl">pixel by pixel</span>
+        </h1>
+      </div>
+      </section>
     </main>
   );
 }
