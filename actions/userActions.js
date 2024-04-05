bdc7210bdee44ba90443a8027ad0e731cecf4a6b
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-04-05 14:43:34 +0300

    created update user image action

--- actions/userActions.js
+++ actions/userActions.js
@@ -1,19 +1,27 @@
 "use server"
 import connectDB from "@/lib/database"
 import User from "@/models/user"
-import { redirect } from "next/navigation";
-import { NextResponse } from "next/server";
 
 export const updateUserName = async (formdata, email) => {
+    try {
+        await connectDB();
+        const result = await User.findOneAndUpdate({ userMail: email }, { fullName: formdata }, { new: true })
+
+        console.log(result);
+        return {}
+
+    } catch (error) {
+        console.log(error);
+        return { error: "Something went wrong" }
+    }
+}
+
+export const updateUserImage = async (imgUrl, email) => {
 
     try {
         await connectDB();
-        // const resul = await User.find({ userMail: "sadikbarisyilmaz@teamrandom.com" }, { new: true })
-        // console.log("resul:", resul[0].id);
-        // const result = await User.findByIdAndUpdate("660ea91e68dc730b3f60642b", { fullName: formdata }, { new: true })
-        // console.log(result);
-        // return {}
-        const result = await User.findOneAndUpdate({ userMail: email }, { fullName: formdata }, { new: true })
+        const result = await User.findOneAndUpdate({ userMail: email }, { img: imgUrl }, { new: true })
+
         console.log(result);
         return {}
     } catch (error) {
--- components/AccountMenu.jsx
+++ components/AccountMenu.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { CircleUser } from "lucide-react";
-import { redirect, useRouter } from "@/navigation";
+import { useRouter } from "@/navigation";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -12,14 +12,24 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { SignOutButton } from "./SignOutButton";
 
-export default function AccountMenu() {
+export default function AccountMenu({ user }) {
   const router = useRouter();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="secondary" size="icon" className="rounded-full">
-          <CircleUser className="h-5 w-5" />
+          {user.image === undefined ? (
+            <CircleUser className="h-5 w-5" />
+          ) : (
+            <Image
+              src={user.image}
+              width={40}
+              height={40}
+              alt="Picture of the author"
+            />
+          )}
+
           <span className="sr-only">Toggle user menu</span>
         </Button>
       </DropdownMenuTrigger>
