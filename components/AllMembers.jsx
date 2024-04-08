606cd4952e71b8f6587863186f9822e93f4b078a
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-04-08 20:15:38 +0300

    members table

--- components/AllMembers.jsx
+++ components/AllMembers.jsx
@@ -1,8 +1,5 @@
 import { getMembers } from "@/actions/memberAction";
 import { Loader2 } from "lucide-react";
-import { Label } from "./ui/label";
-import { Link } from "@/navigation";
-import Image from "next/image";
 import { columns } from "./members-table/columns";
 import { DataTable } from "./members-table/dataTable";
 
@@ -15,54 +12,9 @@ export default async function AllMembers() {
       </div>
     );
   }
-
-  // const memberOmmittedcolumns = columns.slice(1);
-
   return (
     <div className="flex flex-col gap-4 py-4 w-full">
-      <DataTable
-        // members={members}
-        rows={1}
-        columns={columns}
-        data={data}
-        // client={client}
-      />
-      {/* <div className="flex flex-wrap gap-4">
-        {data.map((member, i) => {
-          return (
-            <div key={i} className="flex flex-wrap gap-4 w-full">
-              <div className="flex sm:flex-row  gap-6 w-full">
-                {member.memberImage === undefined ? (
-                  <div className=" w-25 aspect-square bg-gray-800 text-white flex justify-center items-center rounded">
-                    Image
-                  </div>
-                ) : (
-                  <Image
-                    src={member.memberImage}
-                    width={25}
-                    height={25}
-                    priority={false}
-                    alt="Member Picture"
-                    className="aspect-square"
-                  />
-                )}
-                <div className="flex flex-col w-full">
-                  <div className="flex items-center gap-1 w-full">
-                    <Label className="text-md">Name: </Label>
-                    <p className="break-all">{member.memberName}</p>
-                  </div>
-                  <Link
-                    href={`/dashboard/members/${member.id}`}
-                    className="flex items-center gap-1 w-full"
-                  >
-                    See Details
-                  </Link>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div> */}
+      <DataTable rows={1} columns={columns} data={data} />
     </div>
   );
 }
