f29631a19c2d50af6dae0879e09238beeca104b1
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2024-04-09 20:04:54 +0300

    IMPLEMENTATION: NewWorkForm with validations.

---
+++ actions/workAction.js
@@ -0,0 +1,53 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import Work from "@/models/work";
+import connectDB from "@/lib/database";
+import { z } from "zod";
+
+export async function createWork(formData) {
+  const workSchema = z.object({
+    workTitle: z.string().min(2, "Title must be at least 2 characters."),
+    workGithubURL: z.string().url().optional().or(z.literal("")),
+    workAppURL: z.string().url().optional().or(z.literal("")),
+    workReadme: z.string().min(2, "Readme must be at least 2 characters."),
+    workTechStack: z
+      .string()
+      .min(2, "Tech Stack must be at least 2 characters."),
+    workContributors: z.string().optional(), // Assuming contributors is an array of strings
+    // workImages: z.array().url().optional().or(z.literal("")),
+    workImages: z.array(z.string().url()).optional(),
+  });
+
+  const validatedFields = workSchema.safeParse({
+    workTitle: formData.workTitle,
+    workGithubURL: formData.workGithubURL,
+    workAppURL: formData.workAppURL,
+    workReadme: formData.workReadme,
+    workTechStack: formData.workTechStack,
+    workContributors: formData.workContributors,
+    workImages: formData.workImages,
+  });
+  //   const validatedFields = workSchema.safeParse(formData);
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error);
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+  try {
+    await connectDB();
+    const member = await Work.create(validatedFields.data);
+    revalidatePath("/");
+    return {
+      message: `Whoa! ${validatedFields.data.workTitle}, amazing project!`,
+    }; // Return the created member object
+  } catch (error) {
+    console.error("Failed to create work:", error);
+    // Handle database errors, e.g., connection issues or constraints violations
+    return {
+      error: `Failed to create the member due to ${error.message}`,
+    };
+  }
+}
--- components/forms/NewWorkForm.jsx
+++ components/forms/NewWorkForm.jsx
@@ -4,195 +4,224 @@ import { useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useEdgeStore } from "@/lib/edgestore";
 import { Separator } from "@/components/ui/separator";
-import { createMember } from "@/actions/memberAction";
+import { createWork } from "@/actions/workAction";
 import { Loader2 } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
 import { MultiImageDropzone } from "@/components/MultiImageDropzone";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 const NewWorkForm = () => {
   const { edgestore } = useEdgeStore();
   const [pending, setPending] = useState(false);
   const [fileStates, setFileStates] = useState([]);
   const { toast } = useToast();
-  const nameInputRef = useRef(null);
-  const lastNameInputRef = useRef(null);
-  const bioInputRef = useRef(null);
-  const titleInputRef = useRef(null);
-  const personalInputRef = useRef(null);
-  const githubInputRef = useRef(null);
-  const linkedinInputRef = useRef(null);
 
-  //   function updateFileProgress(key, progress) {
-  //     setFileStates((fileStates) => {
-  //       const newFileStates = structuredClone(fileStates);
-  //       const fileState = newFileStates.find(
-  //         (fileState) => fileState.key === key
-  //       );
-  //       if (fileState) {
-  //         fileState.progress = progress;
-  //       }
-  //       return newFileStates;
-  //     });
-  //   }
+  const newWorkSchema = z.object({
+    workTitle: z.string().min(2, {
+      message: "Title must be at least 2 characters.",
+    }),
+    workGithubURL: z.string().url().optional().or(z.literal("")),
+    workAppURL: z.string().url().optional().or(z.literal("")),
+    workReadme: z.string().min(2, {
+      message: "Readme must be at least 2 characters.",
+    }),
+    workTechStack: z.string().min(2, {
+      message: "Tech Stack must be at least 2 characters.",
+    }),
+    workContributors: z.string().optional(),
+    // workImages: z.array().url().optional().or(z.literal("")),
+    // workImages: z.array(z.string().url()).optional(),
+  });
 
-  async function newMember(e) {
-    e.preventDefault();
+  const form = useForm({
+    resolver: zodResolver(newWorkSchema),
+    defaultValues: {
+      workTitle: "",
+      workGithubURL: "",
+      workAppURL: "",
+      workReadme: "",
+      workTechStack: "",
+      workContributors: "",
+      workImages: [],
+    },
+  });
+
+  async function newWork(formData) {
     setPending(true);
-    const name = nameInputRef.current.value;
-    const lastName = lastNameInputRef.current.value;
-    const bio = bioInputRef.current.value;
-    const title = titleInputRef.current.value;
-    const personal = personalInputRef.current.value;
-    const github = githubInputRef.current.value;
-    const linkedin = linkedinInputRef.current.value;
+
     try {
-      if (!file) {
-        throw new Error("Please select a picture to be uploaded.");
-      }
-      const res = await edgestore.publicFiles.upload({
-        file,
-      });
-      // you can run some server action or api here
-      // to add the necessary data to your database
-      const img = res.url;
-      const formData = {
-        memberName: name,
-        memberLastName: lastName,
-        memberBio: bio,
-        memberTitle: title,
-        memberPersonal: personal,
-        memberGithub: github,
-        memberLinkedin: linkedin,
-        memberImage: img,
+      // First, handle the image uploads.
+      const uploadPromises = fileStates.map((addedFileState) =>
+        edgestore.publicFiles.upload({ file: addedFileState.file }).then(res => res.url)
+      );
+
+      // Wait for all uploads to finish.
+      const imageURLs = await Promise.all(uploadPromises);
+
+      // Add the imageURLs to the formData.
+      const dataWithImages = {
+        ...formData,
+        workImages: imageURLs,
       };
-      const response = await createMember(formData); // Assuming createMember can handle the object directly
+
+      // Then, pass this updated data object to your API call function.
+      const response = await createWork(dataWithImages);
       if (response.error) {
         throw new Error(response.error);
       }
-      nameInputRef.current.value = "";
-      lastNameInputRef.current.value = "";
-      bioInputRef.current.value = "";
-      titleInputRef.current.value = "";
-      personalInputRef.current.value = "";
-      githubInputRef.current.value = "";
-      linkedinInputRef.current.value = "";
-      setFile(null);
+
+      // Handle success.
       toast({
-        description: "Member Created !",
+        description: `${response.message}`,
       });
+
+      // Reset form and state as needed.
+      form.reset();
+      setFileStates([]);
+
     } catch (error) {
+      console.error("Error submitting form:", error);
       toast({
-        description: `Error creating member: ${error.message || error}`,
+        description: `Error creating work: ${error.message || error}`,
       });
-      // Handle error, e.g., show error message
     } finally {
-      setPending(false); // Reset pending status
+      setPending(false);
     }
-  }
+}
 
   return (
     <div className="flex gap-4 flex-col-reverse sm:flex-row">
-      <form
-        onSubmit={newMember}
-        className="space-y-8"
-      >
-        <div className="flex flex-wrap gap-4">
-          <div className="flex flex-col w-full md:w-fit gap-2">
-            <Label htmlFor="memberName">First Name</Label>
-            <Input
-              placeholder="First Name"
-              name="memberName"
-              type="text"
-              className="block"
-              ref={nameInputRef}
-              required
-            />
-          </div>
-          <div className="flex flex-col w-full md:w-fit gap-2">
-            <Label htmlFor="memberLastName">Last Name</Label>
-            <Input
-              placeholder="Last Name"
-              name="memberLastName"
-              type="text"
-              ref={lastNameInputRef}
-              required
-            />
-          </div>
-          <div className="flex flex-col w-full md:w-fit gap-2">
-            <Label htmlFor="memberTitle">Title</Label>
-            <Input
-              placeholder="Title"
-              name="memberTitle"
-              type="text"
-              ref={titleInputRef}
-              required
-            />
-          </div>
-        </div>
-        <div className="flex flex-col flex-wrap gap-2 ">
-          <Label htmlFor="memberBio">Bio (Optional)</Label>
-          <Textarea
-            name="memberBio"
-            type="text"
-            className="p-2"
-            ref={bioInputRef}
-          />
-        </div>
-        <Separator />
-
-        <div className="flex flex-col">
-          <h2 className="text-2xl mb-4">Links (Optional)</h2>
-          <div className="flex gap-2 flex-wrap flex-col">
-            <div className="flex flex-col w-full gap-2">
-              <Label htmlFor="memberPersonal">Personal App</Label>
-              <Input
-                placeholder="Personal Link"
-                name="memberPersonal"
-                type="text"
-                ref={personalInputRef}
-              />
-            </div>
-            <div className="flex flex-col w-full gap-2">
-              <Label htmlFor="memberGithub">Github</Label>
-              <Input
-                placeholder="Github Link"
-                name="memberGithub"
-                type="text"
-                ref={githubInputRef}
-              />
-            </div>
-            <div className="flex flex-col w-full gap-2">
-              <Label htmlFor="memberLinkedin">LinkedIn</Label>
-              <Input
-                placeholder="Linkedin Link"
-                name="memberLinkedin"
-                type="text"
-                ref={linkedinInputRef}
-              />
-            </div>
-          </div>
-        </div>
-        <Button
-          type="submit"
-          disabled={pending}
-          className={`w-full`}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(newWork)}
+          className="space-y-8 lg:w-1/2"
         >
-          {pending ? (
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            "Add Member"
-          )}
-        </Button>
-      </form>
-      {/* <p
-        aria-live="polite"
-        className="sr-only"
-      >
-        {state?.message}
-      </p> */}
+          <FormField
+            control={form.control}
+            name="workTitle"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Title"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Separator />
+          <div className="flex flex-col sm:flex-row w-full gap-4">
+            <FormField
+              control={form.control}
+              name="workGithubURL"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Github URL(opt)</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Github URL"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="workAppURL"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>App URL(opt)</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="App URL"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex flex-col sm:flex-row w-full gap-4">
+            <FormField
+              control={form.control}
+              name="workReadme"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Readme</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Readme"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="workTechStack"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Tech Stack</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Tech Stack"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormField
+            control={form.control}
+            name="workContributors"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contributors (Opt)</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Contributors"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            disabled={pending}
+            className={`w-full`}
+          >
+            {pending ? (
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            ) : (
+              "Add new work"
+            )}
+          </Button>
+        </form>
+      </Form>
 
       <div>
         <MultiImageDropzone
@@ -203,32 +232,8 @@ const NewWorkForm = () => {
           onChange={(files) => {
             setFileStates(files);
           }}
-          onFilesAdded={async (addedFiles) => {
+          onFilesAdded={(addedFiles) => {
             setFileStates([...fileStates, ...addedFiles]);
-            await Promise.all(
-              addedFiles.map(async (addedFileState) => {
-                try {
-                  const res = await edgestore.publicFiles.upload({
-                    file: addedFileState.file,
-                    // onProgressChange: async (progress) => {
-                    //   updateFileProgress(addedFileState.key, progress);
-                    //   if (progress === 100) {
-                    //     // wait 1 second to set it to complete
-                    //     // so that the user can see the progress bar at 100%
-                    //     await new Promise((resolve) =>
-                    //       setTimeout(resolve, 1000)
-                    //     );
-                    //     updateFileProgress(addedFileState.key, "COMPLETE");
-                    //   }
-                    // },
-                  });
-                  console.log(res);
-                } catch (err) {
-                //   updateFileProgress(addedFileState.key, "ERROR");
-                console.log(err)
-                }
-              })
-            );
           }}
         />
       </div>
