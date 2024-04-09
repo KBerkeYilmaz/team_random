0c1ac51ed01c838c15d2887fcc6ac6e00d7ee53f
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2024-04-09 15:42:00 +0300

    IMPLEMENTATION: Animated scrollbar.

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,4 +1,5 @@
 import { HeroWavy } from "@/components/HeroWavy";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 
 export default function Home() {
   return (
@@ -12,39 +13,41 @@ export default function Home() {
           span={"one pixel at a time"}
         />
       </section>
-      <section
-        id="about"
-        className="w-full flex h-screen  items-center justify-center"
-      >
-        <div className="w-fit h-fit flex flex-wrap flex-col justify-start items-center p-4">
-          <h2 className="permanent-marker-bold text-3xl sm:text-4xl md:text-6xl dark:text-orange-500">
-            This is about section(kinda){" "}
-            <span className="pixelify-header-bold">pixel by pixel</span>
-          </h2>
-        </div>
-      </section>
-      <section
-        id="services"
-        className="w-full flex h-screen  items-center justify-center"
-      >
-        <div className="w-fit h-fit flex flex-wrap flex-col justify-start items-center p-4">
-          <h2 className="permanent-marker-bold text-3xl sm:text-4xl md:text-6xl dark:text-orange-500">
-            This is services section{" "}
-            <span className="pixelify-header-bold">pixel by pixel</span>
-          </h2>
-        </div>
-      </section>
-      <section
-        id="cta"
-        className="w-full flex h-screen items-center justify-center"
-      >
-        <div className="w-fit h-fit flex flex-wrap flex-col justify-start items-center p-4">
-          <h2 className="permanent-marker-bold text-3xl sm:text-4xl md:text-6xl dark:text-orange-500">
-            This is cta{" "}
-            <span className="pixelify-header-bold">pixel by pixel</span>
-          </h2>
-        </div>
-      </section>
+      <TracingBeam>
+        <section
+          id="about"
+          className="w-full flex h-screen  items-center justify-center"
+        >
+          <div className="w-fit h-fit flex flex-wrap flex-col justify-start items-center p-4">
+            <h2 className="permanent-marker-bold text-3xl sm:text-4xl md:text-6xl dark:text-orange-500">
+              This is about section(kinda){" "}
+              <span className="pixelify-header-bold">pixel by pixel</span>
+            </h2>
+          </div>
+        </section>
+        <section
+          id="services"
+          className="w-full flex h-screen  items-center justify-center"
+        >
+          <div className="w-fit h-fit flex flex-wrap flex-col justify-start items-center p-4">
+            <h2 className="permanent-marker-bold text-3xl sm:text-4xl md:text-6xl dark:text-orange-500">
+              This is services section{" "}
+              <span className="pixelify-header-bold">pixel by pixel</span>
+            </h2>
+          </div>
+        </section>
+        <section
+          id="cta"
+          className="w-full flex h-screen items-center justify-center"
+        >
+          <div className="w-fit h-fit flex flex-wrap flex-col justify-start items-center p-4">
+            <h2 className="permanent-marker-bold text-3xl sm:text-4xl md:text-6xl dark:text-orange-500">
+              This is cta{" "}
+              <span className="pixelify-header-bold">pixel by pixel</span>
+            </h2>
+          </div>
+        </section>
+      </TracingBeam>
     </main>
   );
 }
