ef72ad25e2069806fb61488898ff2f8f173fe286
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2024-04-11 15:12:16 +0300

    UPDATE: Loading state for works.

--- app/[locale]/(dashboard)/dashboard/works/loading.jsx
+++ app/[locale]/(dashboard)/dashboard/works/loading.jsx
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 const loading = () => {
   return (
-    <div className="w-full h-screen -mt-[60px]  flex justify-center items-center">
+    <div className="w-full h-screen -mt-[60px] flex justify-center items-center">
       <Loader2 className=" animate-spin" />
     </div>
   );
--- app/[locale]/(dashboard)/dashboard/works/page.jsx
+++ app/[locale]/(dashboard)/dashboard/works/page.jsx
@@ -3,100 +3,18 @@ import { columns } from "./columns";
 import DataTable from "@/components/ui/data-table";
 import { Separator } from "@/components/ui/separator";
 import { getWorks } from "@/actions/workAction";
-
-// const data = [
-//   {
-//     id: 123455,
-//     title: "test1",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test2",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test3",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test4",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test5",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-//   {
-//     id: 123455,
-//     title: "test",
-//     githubUrl: "www.test.com",
-//     appUrl: "www.test.com",
-//     stack: "test",
-//     readme: "test",
-//   },
-// ];
+import { Loader2 } from "lucide-react";
 
 const Works = async () => {
   const res = (await getWorks());
+  
+  if (!res) {
+    return (
+      <div className="flex w-full justify-center mt-10">
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      </div>
+    );
+  }
 
   console.log("Response from server!!!:" , res);
   const data = res.map(item => ({
