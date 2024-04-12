54a9e4ecd7bb0eddb7e608a752fb8b19752cb566
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-04-12 13:30:28 +0300

    Implementation: LoginForm  hook-form implementation

--- components/forms/LoginForm.jsx
+++ components/forms/LoginForm.jsx
@@ -5,20 +5,29 @@ import { useRouter } from "@/navigation";
 import { Button } from "../ui/button";
 import { Loader2 } from "lucide-react";
 import { useToast } from "../ui/use-toast";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Separator } from "../ui/separator";
+import { Input } from "../ui/input";
 
 const LoginForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
   const { toast } = useToast();
-  const handlePasswordChange = (e) => setPassword(e.target.value);
-  const handleEmailChange = (e) => setEmail(e.target.value);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const onSubmit = async (values) => {
     setIsSubmitting(true);
     // Use Next-Auth's signIn function instead of fetch
+    const { email, password } = values;
     const result = await signIn("credentials", {
       redirect: false, // Prevent Next-Auth from redirecting automatically
       email,
@@ -45,49 +54,68 @@ const LoginForm = () => {
     }
   };
 
-  return (
-    <form
-      className="sign-in-form lg:px-[8.15rem] w-screen flex justify-center  items-center max-w-[1024px]"
-      id="sign-in-form"
-      onSubmit={handleSubmit}
-    >
-      <div className="py-[15%] px-[1.87rem] flex flex-col gap-[8px] w-full">
-        <h3 className="pb-[0.9rem] font-bold text-[35px] text-left">Login</h3>
+  const formSchema = z.object({
+    email: z.string().email("Please enter a valid email."),
+    password: z.string().min(3, "Password must be at least 3 characters."),
+  });
 
-        <div className="grid grid-rows-2 grid-cols-1 gap-3 mb-4">
-          <div className="w-full">
-            <label htmlFor="user-email">E-Mail</label>
-            <input
-              id="user-email"
-              type="email"
-              onChange={handleEmailChange}
-              name="email"
-              className="w-full rounded-md bg-input border h-[40px] p-[0.75rem]"
-            />
-          </div>
-          <div>
-            <label htmlFor="user-password">Password</label>
-            <input
-              id="user-password"
-              type="password"
-              name="password"
-              className="w-full rounded-md bg-input border h-[40px] p-[0.75rem]"
-              onChange={handlePasswordChange}
-            />
-          </div>
-        </div>
-        {!isSubmitting ? (
-          <Button id="form-submit-btn" type="submit">
-            Login
-          </Button>
-        ) : (
-          <Button disabled>
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-            Please wait
-          </Button>
-        )}
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <div className="flex flex-col gap-4 w-full max-w-xl">
+      <h2 className="text-5xl font-semibold">Login</h2>
+      <Separator />
+      <div className="flex flex-col gap-2">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="">
+            <div className="grid  gap-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            {!isSubmitting ? (
+              <Button className="w-full mt-2" type="submit">
+                Submit
+              </Button>
+            ) : (
+              <Button className="w-full mt-2" disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            )}
+          </form>
+        </Form>
       </div>
-    </form>
+    </div>
   );
 };
 
