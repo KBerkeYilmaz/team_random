1d79012b9b99abb1b7bb0fc593c26de553eb070f
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-04-15 17:43:38 +0300

    Feature: Added home link to dashboard sidebar mobile DashboardSidebarNavMobile component

--- components/DashboardSidebarNavMobile.jsx
+++ components/DashboardSidebarNavMobile.jsx
@@ -1,16 +1,22 @@
 "use client";
 
 import { Link, usePathname } from "@/navigation";
-import { Home, Package, ShoppingCart, Users } from "lucide-react";
+import { Home, Package, Package2, ShoppingCart, Users } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { SignOutButton } from "@/components/SignOutButton";
 import { SheetClose } from "./ui/sheet";
+import { Separator } from "./ui/separator";
 
 export default function DashboardSidebarNavMobile() {
   const pathname = usePathname();
 
   return (
     <nav className="grid items-start px-2 text-sm font-medium  gap-2">
+      <Link href="/" className="flex items-center gap-2 font-semibold">
+        <Package2 className="h-6 w-6 m-2" />
+        <span className="sr-only">Acme Inc</span>
+      </Link>
+      <Separator />
       <Link
         href="/dashboard"
         className={` transition-all ease-in-out duration-200 flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground ${
