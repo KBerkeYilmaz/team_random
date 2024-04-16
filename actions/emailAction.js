a2dd4f987ffd847be664b5b74d8bfd0b282df511
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2024-04-16 22:56:22 +0300

    UPDATE: Gmail integration for inbox, v.1

---
+++ actions/emailAction.js
@@ -0,0 +1,17 @@
+"use server";
+
+export async function fetchInbox() {
+  //   const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000"; // Define the base URL
+  const baseUrl = "http://localhost:3000"; // Define the base URL
+  const res = await fetch(`${baseUrl}/api/email`); // Use the full URL to fetch
+  const data = await res.json(); // Convert the response to JSON
+  const emails = data || []; // Ensure emails is always an array
+  // Check if the fetch was successful
+  if (!res.ok) {
+    console.error("Failed to fetch emails:", data);
+    return { props: { emails: [] } }; // Return empty array on failure
+  }
+
+  // Return the fetched emails as props to the component
+  return emails; // Ensure emails is always an array
+}
--- app/[locale]/(dashboard)/dashboard/messages/page.jsx
+++ app/[locale]/(dashboard)/dashboard/messages/page.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use server";
+import { Loader2 } from "lucide-react";
+import { fetchInbox } from "@/actions/emailAction";
+import { Separator } from "@/components/ui/separator";
 
-function page() {
-  return <div>page</div>;
+async function page() {
+  const data = await fetchInbox();
+  if (!data) {
+    return (
+      <div className="flex w-full justify-center mt-10">
+        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+      </div>
+    );
+  }
+  return (
+    <div className="h-full w-full p-10 animate-fadeIn">
+      <h1 className="text-4xl font-semibold">Inbox</h1>
+      <Separator className="my-4" />
+      <ul className="flex flex-col gap-4 py-4 w-full">
+        {data.map((email, index) => (
+          <li
+            key={index}
+          >
+            <span>{email.uid}</span>
+            <h3>{email.subject}</h3>
+            <p>{email.date}</p>
+            <p>{email.from}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default page;
