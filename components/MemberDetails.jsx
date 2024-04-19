fc303c96938457708b937501750da2f2ff82725e
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-04-19 13:09:19 +0300

    Styling: our servisec section made responsive

--- components/MemberDetails.jsx
+++ components/MemberDetails.jsx
@@ -63,26 +63,20 @@ export default function MemberDetails({ member }) {
           <div className="flex flex-col w-full gap-2">
             <div className="grid grid-cols-2 gap-2 md:grid-cols-1">
               <div className="flex gap-1 w-full flex-col md:flex-row">
-                <Label className="text-md font-bold dark:text-primary ">
-                  Name:{" "}
-                </Label>
+                <Label className="text-md font-bold">Name: </Label>
                 <p className="break-all sm:text-start text-justify">
                   {member.memberName}
                 </p>
               </div>
               <div className="flex gap-1 w-full flex-col md:flex-row">
-                <Label className="text-md font-bold dark:text-primary ">
-                  Title:{" "}
-                </Label>
+                <Label className="text-md font-bold">Title: </Label>
                 <p className="break-all sm:text-start text-justify">
                   {member.memberTitle}
                 </p>
               </div>
               {member.memberPersonal && (
                 <div className="flex gap-1 w-full flex-col md:flex-row">
-                  <Label className="text-md font-bold dark:text-primary ">
-                    Personal:{" "}
-                  </Label>
+                  <Label className="text-md font-bold">Personal: </Label>
                   <p className="break-all sm:text-start text-justify">
                     {" "}
                     <a href={member.memberPersonal}> {member.memberPersonal}</a>
@@ -91,9 +85,7 @@ export default function MemberDetails({ member }) {
               )}
               {member.memberLinkedin && (
                 <div className="flex gap-1 w-full flex-col md:flex-row">
-                  <Label className="text-md font-bold dark:text-primary ">
-                    Linkedin:{" "}
-                  </Label>
+                  <Label className="text-md font-bold">Linkedin: </Label>
                   <p className="break-all sm:text-start text-justify">
                     {" "}
                     <a href={member.memberLinkedin}> {member.memberLinkedin}</a>
@@ -102,9 +94,7 @@ export default function MemberDetails({ member }) {
               )}
               {member.memberGithub && (
                 <div className="flex gap-1 w-full flex-col md:flex-row">
-                  <Label className="text-md font-bold dark:text-primary ">
-                    Github:{" "}
-                  </Label>
+                  <Label className="text-md font-bold">Github: </Label>
                   <p className="break-all sm:text-start text-justify">
                     <a href={member.memberGithub}> {member.memberGithub}</a>
                   </p>
@@ -112,9 +102,7 @@ export default function MemberDetails({ member }) {
               )}
             </div>
             <div className="flex gap-1 w-full flex-col md:flex-row">
-              <Label className="text-md font-bold dark:text-primary ">
-                Bio:{" "}
-              </Label>
+              <Label className="text-md font-bold">Bio: </Label>
               <p className="break-all sm:text-start text-justify">
                 {member.memberBio}
               </p>
