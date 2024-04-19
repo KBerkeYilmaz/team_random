8258c7d85cb2e180729e4a6113abfab7f65cc8c3
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-04-19 18:39:50 +0300

    Feature: edip password form created

--- components/MyAccount.jsx
+++ components/MyAccount.jsx
@@ -5,6 +5,7 @@ import { EditUserForm } from "@/components/forms/EditUserForm";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { Loader2 } from "lucide-react";
+import { EditUserPasswordForm } from "./forms/EditUserPasswordForm";
 
 function MyAccount() {
   const { data } = useSession();
@@ -17,7 +18,7 @@ function MyAccount() {
     );
   }
   return (
-    <div className="h-full w-full flex flex-col gap-4 justify-start items-center md:items-start p-10 animate-fadeIn overflow-y-scroll no-scrollbar">
+    <div className="h-screen w-full flex flex-col gap-4 justify-start items-center md:items-start pb-28 p-10 animate-fadeIn overflow-y-scroll no-scrollbar">
       <h2 className="text-4xl font-semibold text-center sm:text-start ">
         My Account
       </h2>
@@ -52,8 +53,9 @@ function MyAccount() {
             </div>
           </div>
         </div>
-        <div className="flex w-full">
+        <div className="flex w-full  flex-wrap">
           <EditUserForm user={data?.user} />
+          <EditUserPasswordForm user={data?.user} />
         </div>
       </div>
     </div>
---
+++ components/forms/EditUserPasswordForm.jsx
@@ -0,0 +1,140 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Loader2 } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  currentPassword: z.string().min(3),
+  newPassword: z.string().min(3),
+  passwordConfirmation: z.string().min(3),
+});
+
+import { useToast } from "../ui/use-toast";
+
+export const EditUserPasswordForm = ({ user }) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { toast } = useToast();
+  console.log(user);
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      currentPassword: "",
+      newPassword: "",
+      passwordConfirmation: "",
+    },
+  });
+
+  const onSubmit = async (values) => {
+    console.log(values);
+    if (user.role !== "admin") {
+      toast({
+        variant: "destructive",
+        title: "Error !",
+        description: `- Unauthorised !`,
+      });
+      return;
+    }
+    setIsSubmitting(true);
+    // const result = await updateUser(values, user.id, user.role);
+
+    // if (result.error) {
+    //   toast({
+    //     variant: "destructive",
+    //     title: "Error !",
+    //     description: `- ${result.error}`,
+    //   });
+
+    //   setIsSubmitting(false);
+    // } else {
+    //   toast({
+    //     title: "Update Successful !",
+    //   });
+    //   setIsSubmitting(false);
+    // }
+  };
+
+  return (
+    <div className="flex flex-col gap-4 w-fit">
+      <h2 className="text-3xl font-semibold md:text-start text-center">
+        Edit Password
+      </h2>
+      <Separator />
+      <div className="flex flex-col gap-2 max-w-2xl">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="">
+            <div className="grid  gap-2">
+              <FormField
+                control={form.control}
+                name="currentPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-bold">
+                      Current Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="newPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-bold">New Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="passwordConfirmation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-bold">
+                      Confirm Password
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            {!isSubmitting ? (
+              <Button className="w-full mt-2" type="submit">
+                Submit
+              </Button>
+            ) : (
+              <Button className="w-full mt-2" disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            )}
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
