a829a7a31dc62d4d8cabcc984f4867bd4c4c371e
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2024-04-19 18:26:33 +0300

    UPDATE: Dark theme changes

--- components/DashboardSidebar.jsx
+++ components/DashboardSidebar.jsx
@@ -13,7 +13,7 @@ const DashboardSidebar = async ({ children }) => {
 
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
+      <div className="hidden border-r dark:bg-background bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
@@ -30,7 +30,7 @@ const DashboardSidebar = async ({ children }) => {
       </div>
 
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <header className="flex h-14 items-center gap-4 border-b dark:bg-background bg-muted/40 px-4 lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button
