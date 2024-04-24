b2ee485af2aad6d6e0fc72223b8e1bc88a50aefa
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2024-04-24 14:56:15 +0300

    UPDATE: Footer implementation.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
     <footer className="h-fit w-screen">
       <div className="flex h-full w-full items-center justify-center bg-background p-20">
         <div className="flex h-full w-full items-center justify-center">
-          <p className="text-center text-sm font-semibold ">
+          <p className="text-center text-sm font-semibold">
             Team Random &copy; 2024. All rights reserved.
           </p>
         </div>
