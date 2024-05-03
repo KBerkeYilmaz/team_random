3e6dcbbbdc6052215e81c45f019f679696e51dd7
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2024-05-03 16:19:01 +0300

    Fix: Changed loading state component width to "full" from "screen"

--- app/[locale]/login/loading.jsx
+++ app/[locale]/login/loading.jsx
@@ -1,11 +1,11 @@
-import { Loader2 } from "lucide-react"
+import { Loader2 } from "lucide-react";
 
 const loading = () => {
-    return ( 
-        <div className="w-screen h-screen flex justify-center items-center z-50">
-            <Loader2 className="mr-2 w-1/2 animate-spin" />
-        </div>
-    )
-}
+  return (
+    <div className="w-full h-screen flex justify-center items-center z-50">
+      <Loader2 className="mr-2 w-1/2 animate-spin" />
+    </div>
+  );
+};
 
 export default loading;
